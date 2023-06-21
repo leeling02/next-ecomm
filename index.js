891b2f32a8efda8079f21fae09dae5d901fd5f60
Tan Lee Ling <tanleeling98@gmail.com>
2023-06-21 17:55:54 +0800

    error-handling,filtering-return-values,validate-inputs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import prisma from "./src/utils/prisma.js"
+import { Prisma } from "@prisma/client"
 
 const app = express()
 
@@ -15,6 +16,11 @@ app.get('/', async (req, res) => {
   res.json(allUsers)
 })
 
+
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
+}
+
 // app.post(`/user`, async (req, res) => {
 //     const { name, email, password } = req.body
 //     const result = await prisma.user.create({
@@ -27,15 +33,59 @@ app.get('/', async (req, res) => {
 //     res.json(result)
 //   })
 
+function validateUser(input) {
+  const validationErrors = {}
+
+  if (!('name' in input) || input['name'].length == 0) {
+    validationErrors['name'] = 'cannot be blank'
+  }
+
+  if (!('email' in input) || input['email'].length == 0) {
+    validationErrors['email'] = 'cannot be blank'
+  }
+
+  if (!('password' in input) || input['password'].length == 0) {
+    validationErrors['password'] = 'cannot be blank'
+  } 
+
+  if ('password' in input && input['password'].length < 8) {
+    validationErrors['password'] = 'should be at least 8 characters'
+  }
+
+  if ('email' in input && !input['email'].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    validationErrors['email'] = 'is invalid'
+  }
+
+  return validationErrors
+}
+
 app.post('/users', async (req, res) => {
-    const data = req.body
-  
-    prisma.user.create({
-      data,
-    }).then(user => {
-      return res.json(user)
-    })
+  const data = req.body
+  const validationErrors = validateUser(data)
+
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
   })
+
+
+  prisma.user.create({
+    data
+  }).then(user => {
+    return res.json(filter(user, 'id','name','email'))
+
+  }).catch(err => {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      });  // friendly error handling
+    }
+    throw err  // if this happens, our backend application will crash and not respond to the client. because we don't recognize this error yet, we don't know how to handle it in a friendly manner. we intentionally throw an error so that the error monitoring service we'll use in production will notice this error and notify us and we can then add error handling to take care of previously unforeseen errors.
+  })
+})
+
 
 //post is used when there is a requirement to fecth data from the client side
 //find the user which match the specified email 
